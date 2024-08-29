f81922cda4a45ca7a41920d9863f6a6efde4fbff
SDeVuyst <73061845+SDeVuyst@users.noreply.github.com>
2024-08-29 21:27:21 +0200

    new logo for collectibles

--- pokemon/urls.py
+++ pokemon/urls.py
@@ -1,9 +1,11 @@
+from django.contrib import admin
 from django.conf.urls.i18n import i18n_patterns
 from django.utils.translation import gettext_lazy as _
 from django.urls import include, path
 from . import views
 
 urlpatterns = i18n_patterns(
+    path('admin/', admin.site.urls),
     path('', views.index, name="index"),
     path(_('over-ons'), views.over_ons, name="over_ons"),
     path(_('contact'), views.contact, name='contact'),
