97818d3855eddaf40ff4dfa35d98705be81f3dcd
SDeVuyst <silasdevuyst@hotmail.com>
2025-11-25 11:26:15 +0100

    dev files

---
+++ darts/middleware.dev.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponseNotFound
+
+class DomainMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        host = request.get_host().split(':')[0]
+        
+        if host == 'chudartz.com':
+            request.urlconf = 'darts.urls'
+        elif host == 'chudartz-collectibles.com':
+            request.urlconf = 'pokemon.urls'
+        else:
+            request.urlconf = 'darts.urls'
+            # return HttpResponseNotFound('Domain not recognized')
+        
+        response = self.get_response(request)
+        return response
